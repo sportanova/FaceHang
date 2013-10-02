c4017bb3b5c1ad992d3b46258a1da4623192c61a
Sportanova <sportano@gmail.com>
2013-10-02 02:46:11 -0700

    revealing letters when guessed correctly

--- public/app/models/guessLetter-model.js
+++ public/app/models/guessLetter-model.js
@@ -5,13 +5,17 @@ App.Models.GuessLetter = Backbone.Model.extend({
   },
 
   analyzeGuess: function(letter) {
+    var guessedRight = false;
+
     var letters = this.gameWord.letters.models;
     for(var i = 0; i < letters.length; i++) {
       if(letters[i].get('letter') === letter) {
+        guessedRight = true;
         console.log('you got it right');
       } else {
         console.log(letters[i].get('letter'),'thats a strike');
       }
     }
+    return guessedRight;
   }
 });
--- public/app/views/guessLetter-view.js
+++ public/app/views/guessLetter-view.js
@@ -2,13 +2,15 @@ App.Views.GuessLetter = Backbone.View.extend({
   template: _.template(App.Templates.GuessLetter),
 
   events: {
-    'click .submitGuess': 'captureLetter'
+    'click .submitGuess': 'captureLetter',
   },
 
   captureLetter: function() {
     var letterGuessed = $('.inputGuess').val();
     this.onlyOneLetter(letterGuessed);
     this.model.analyzeGuess(letterGuessed);
+
+    this.revealCorrectLetters(letterGuessed);
   },
 
   onlyOneLetter: function(input) {
@@ -16,6 +18,19 @@ App.Views.GuessLetter = Backbone.View.extend({
       alert('need to enter one and only one letter');
       return false;
     }
+  },
+
+  revealCorrectLetters: function(guessedLetter) {
+    guessedLetter = guessedLetter.toLowerCase();
+
+    $('.letter').each(function(index, letter) {
+      var $this = $(this);
+      var thisLetter = $this.text().toLowerCase();
+
+      if(thisLetter === guessedLetter) {
+        $this.css({'display': 'inline-block'});
+      }
+    });
   },
 
   render: function() {

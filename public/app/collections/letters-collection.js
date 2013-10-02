cc76e77ae4a2207c44c2fe4fe1388b030fb68093
Sportanova <sportano@gmail.com>
2013-10-02 00:27:39 -0700

    split random last name into new collection

---
+++ public/app/collections/letters-collection.js
@@ -0,0 +1,3 @@
+App.Collections.Letters = Backbone.Collection.extend({
+
+});
--- public/app/models/hangman-model.js
+++ public/app/models/hangman-model.js
@@ -5,8 +5,22 @@ App.Models.Hangman = Backbone.Model.extend({
   },
 
   startGame: function() {
+    this.pickName();
+    this.splitName();
+  },
+
+  pickName: function() {
     var randomPick = Math.floor(Math.random() * this.friendsCollection.length);
+
     this.name = this.friendsCollection.at(randomPick).get('name').split(' ');
     this.gameWord = this.name[this.name.length - 1];
+  },
+
+  splitName: function() {
+    this.letters = new App.Collections.Letters();
+
+    for(var i = 0; i < this.gameWord.length; i++) {
+      this.letters.add(this.gameWord[i]);
+    }
   }
 });

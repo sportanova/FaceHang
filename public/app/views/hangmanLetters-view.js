61f1bf45d49ce70f1b5c35d3918c1f7b0aa3b7dd
Sportanova <sportano@gmail.com>
2013-10-02 22:02:17 -0700

    reveals bodyparts when wrong answer; rehides body after game

--- public/app/views/hangmanLetters-view.js
+++ public/app/views/hangmanLetters-view.js
@@ -1,9 +1,23 @@
 App.Views.HangmanLetters = Backbone.View.extend({
   initialize: function() {
     this.model.on('endGame', this.render, this);
+    this.model.on('endGame', this.resetBodyParts, this);
+    this.model.on('addBodyPart', this.addBodyPart, this);
+    this.bodyParts = ['.head', '.back', '.arm.left', '.arm.right', '.leg.left', '.leg.right'];
+    this.bodyPartsIndex = 0;
   },
 
   tagName: 'ul',
+
+  addBodyPart: function() {
+    $(this.bodyParts[this.bodyPartsIndex]).css({'display': 'block'});
+    this.bodyPartsIndex++;
+  },
+
+  resetBodyParts: function() {
+    this.bodyPartsIndex = 0;
+    $('.bodyPart').css({'display': 'none'});
+  },
 
   render: function() {
     this.$el.html(
